08ecee6834417cadb23a9c7d790e0c7922097357
TuanTran <tuantranit12@gmail.com>
2022-09-20 07:57:50 +0700

    Add Genre, Category, Country domain model

---
+++ server/models/category.model.js
@@ -0,0 +1,10 @@
+class Category {
+  constructor(id, name, updateAt, createdAt) {
+    this.id = id;
+    this.name = name;
+    this.updateAt = updateAt;
+    this.createdAt = createdAt;
+  }
+}
+
+module.exports = Category;
---
+++ server/models/country.model.js
@@ -0,0 +1,10 @@
+class Country {
+  constructor(id, name, updateAt, createdAt) {
+    this.id = id;
+    this.name = name;
+    this.updateAt = updateAt;
+    this.createdAt = createdAt;
+  }
+}
+
+module.exports = Country;
