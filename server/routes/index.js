194805ef5eab9658b5bb8393ab87a8b5a32137a6
TuanTran <tuantranit12@gmail.com>
2022-09-28 15:57:35 +0700

    Add: 404 page

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,26 @@ function route(app) {
   });
   AdminRoute(app);
   ClientRoute(app);
+
+  /**
+   * Handle error 404
+   */
+  app.use((req, res, next) => {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      return res.render('404', { layout: false });
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+      return res.json({ error: 'Not found' });
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
   app.use((err, req, res, next) => {
     errorHandling(err, res);
   });
