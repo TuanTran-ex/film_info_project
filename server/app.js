31859370394eb12750ef96542abd27ce0d966096
TuanTran <tuantranit12@gmail.com>
2023-02-11 14:39:51 +0700

    Delete unused file in production

--- server/app.js
+++ server/app.js
@@ -10,13 +10,19 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 require('./utils/prototype');
 const route = require('./routes');
+const { allowedDomains } = require('./configs');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('layout', './admin/layout/_layout');
 app.set('layout extractScripts', true);
 
-app.use(cors());
+app.use(
+  cors({
+    methods: ['GET', 'PATCH', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
